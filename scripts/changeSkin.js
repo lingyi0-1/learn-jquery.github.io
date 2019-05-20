dc9bf9b85a8ecab51343844682736565079a0346
lingyi0-1 <1695877968@qq.com>
2019-05-20 12:29:27 +0800

    提交项目

---
+++ scripts/changeSkin.js
@@ -0,0 +1,16 @@
+function switchSkin(skinName){
+	$("#"+skinName).addClass("selected")
+		.siblings().removeClass("selected");
+	$("#cssfile").attr("href","styles/skin/"+ skinName +".css");
+	$.cookie("MyCssSkin" , skinName , {path: '/',expires: 10});
+}
+$(function(){
+	var $li=$("#skin li");
+	$li.click(function(){
+		switchSkin(this.id);
+	});
+	var cookie_skin = $.cookie("MyCssSkin");
+	if(cookie_skin){
+		switchSkin(cookie_skin);
+	}
+})
